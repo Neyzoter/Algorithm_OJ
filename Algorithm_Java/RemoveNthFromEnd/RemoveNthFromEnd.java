22ec44451b8f3e26df43c5e49435adaecb3b00d9
Neyzoter <sonechaochao@gmail.com>
2018-09-16 15:56:51 +0800

    add print

--- Algorithm_Java/RemoveNthFromEnd/RemoveNthFromEnd.java
+++ Algorithm_Java/RemoveNthFromEnd/RemoveNthFromEnd.java
@@ -1,13 +1,27 @@
 
 public class RemoveNthFromEnd {
 	public static void main(String[] args) {
+		ListNode_RemoveNthFromEnd head = new ListNode_RemoveNthFromEnd(1);
+		head.next = new ListNode_RemoveNthFromEnd(2);
+		head.next.next = new ListNode_RemoveNthFromEnd(3);
+		head.next.next.next = new ListNode_RemoveNthFromEnd(4);
+		head.next.next.next.next = new ListNode_RemoveNthFromEnd(5);
+		Solution_RemoveNthFromEnd sol = new Solution_RemoveNthFromEnd();
 		
+		ListNode_RemoveNthFromEnd lstnd = sol.removeNthFromEnd(head,2);
+		while(lstnd!=null) {
+			System.out.printf("%d ",lstnd.val);
+			lstnd = lstnd.next;
+			if(lstnd!=null) {
+				System.out.printf("-> ");
+			}
+		}
 	}
 }
 
 class Solution_RemoveNthFromEnd {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode del_node = head,pointer = head;
+    public ListNode_RemoveNthFromEnd removeNthFromEnd(ListNode_RemoveNthFromEnd head, int n) {
+        ListNode_RemoveNthFromEnd del_node = head,pointer = head;
         for(int idx = 0;idx < n;idx++) {
         	pointer = pointer.next;//将pointer指向要删除的节点的后面第n个
         }
@@ -25,8 +39,8 @@ class Solution_RemoveNthFromEnd {
 }
 
 
-class ListNode {
+class ListNode_RemoveNthFromEnd {
     int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
+    ListNode_RemoveNthFromEnd next;
+    ListNode_RemoveNthFromEnd(int x) { val = x; }
 }
